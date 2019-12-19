0d68fc0972770ea59c7e74c03197571ee8be08c6
rossipilleiktkhk <pille.rossi@khk.ee>
2019-12-19 11:39:27 +0200

    deleted plugin `readme.txt` (after activation of `Weglot Translate` version 3.1.4)

--- wp-content/plugins/bookings/js/admin.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function() {
-	var height = jQuery(window).height() - jQuery('#wpadminbar').height() - jQuery('#wpfooter').height() - 80;
-	jQuery('#bookings-frame').height(height);
-});
